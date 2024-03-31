a415755ccf6d296f4f39d87aa7ddcf067640610e
DmShko <vombat_schev@i.ua>
2024-03-31 21:52:56 +0300

    temp_map-18

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { store, persistor } from './tmStore/index.ts'
 
 
 import App from './App.tsx'
-// import './index.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
